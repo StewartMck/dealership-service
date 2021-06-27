67f7065e7a86fc91be459fbb4489383a00fd66f9
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-06-27 15:27:34 -0600

    Initial commit

---
+++ src/test/java/ca/smckinlay/dealership/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.smckinlay.dealership.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
