7f3b812bb11d0ce3f94391bcc4b2b8ba02ff300e
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-06-30 17:10:30 -0600

    Add edit and new sections & css

--- src/main/java/ca/smckinlay/dealership/service/controllers/CustomErrorController.java
+++ src/main/java/ca/smckinlay/dealership/service/controllers/CustomErrorController.java
@@ -1,47 +1,43 @@
 package ca.smckinlay.dealership.service.controllers;
 
 import ca.smckinlay.dealership.service.services.HTTPStatusErrorService;
-import ca.smckinlay.dealership.service.services.HTTPStatusErrorServiceImpl;
 import ca.smckinlay.dealership.service.util.AttributeNames;
 import ca.smckinlay.dealership.service.util.Mappings;
 import ca.smckinlay.dealership.service.util.Views;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping(Mappings.ERROR)
-public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+public class CustomErrorController implements ErrorController {
 
-    private static final Logger log = LoggerFactory.getLogger(ErrorController.class);
+    private static final Logger log = LoggerFactory.getLogger(CustomErrorController.class);
     private final HTTPStatusErrorService httpStatusErrorService;
 
     @Autowired
-    public ErrorController(HTTPStatusErrorService httpStatusErrorService) {
+    public CustomErrorController(HTTPStatusErrorService httpStatusErrorService) {
         this.httpStatusErrorService = httpStatusErrorService;
     }
 
-    //    @ResponseBody
-    @GetMapping
-    public ModelAndView handleError(HttpServletRequest request){
+    @GetMapping(Mappings.ERROR)
+    public String handleError(HttpServletRequest request, Model model){
 
         int statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        ModelAndView errorPage = new ModelAndView(Views.ERROR);
-        errorPage.addObject(AttributeNames.STATUS_CODE, statusCode);
-        errorPage.addObject(AttributeNames.STATUS_MESSAGE,httpStatusErrorService.getHTTPStatusMsg(statusCode));
+
+        model.addAttribute(AttributeNames.STATUS_CODE, statusCode);
+        model.addAttribute(AttributeNames.STATUS_MESSAGE,httpStatusErrorService.getHTTPStatusMsg(statusCode));
 
         log.error("THIS IS AN ERROR, {}", statusCode);
-        return errorPage;
+        return "error";
     }
 
 }
--- src/main/java/ca/smckinlay/dealership/service/util/Mappings.java
+++ src/main/java/ca/smckinlay/dealership/service/util/Mappings.java
@@ -4,7 +4,7 @@ package ca.smckinlay.dealership.service.util;
 public final class Mappings {
 
     public static final String ERROR = "error";
-    public static final String SERVICE = "/";
+    public static final String SERVICE = "service";
 
     public Mappings() {
     }
