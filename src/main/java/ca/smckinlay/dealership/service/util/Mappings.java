1467bc1cfc3e0a28ce576fb32245f8e8a9b16612
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-07-07 10:20:38 -0600

    Refactor route to service and set nav link

--- src/main/java/ca/smckinlay/dealership/service/util/Mappings.java
+++ src/main/java/ca/smckinlay/dealership/service/util/Mappings.java
@@ -4,7 +4,7 @@ package ca.smckinlay.dealership.service.util;
 public final class Mappings {
 
     public static final String ERROR = "error";
-    public static final String SERVICE = "services";
+    public static final String SERVICE = "service";
     public static final String SERVICE_ITEM = "item";
     public static final String REDIRECT_SERVICES = "redirect:/" + SERVICE;
     public static final String NEW_SERVICE = "new";
