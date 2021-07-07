e01eece612621b08a2cdfb32ee3d64a9dffaf347
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-07-07 13:48:47 -0600

    Add new note section to workshop

--- src/main/resources/static/js/workshop.js
+++ src/main/resources/static/js/workshop.js
@@ -2,16 +2,19 @@
 const expandButton = document.getElementById("expand");
 const table = document.getElementsByClassName("vehicles")[0];
 const edit = document.getElementsByClassName("edit-container")[0];
+const newNote = document.getElementById("new-note");
 
 expandButton.addEventListener("click", ()=>{
     if(expandButton.getAttribute("data_expanded") === 'false') {
         table.style.display = "none";
-        edit.style.width = "100%";
+        edit.style.width = "80%";
         expandButton.setAttribute("data_expanded", true);
+        newNote.style.display = "flex";
     } else {
         table.style.display = "table";
         edit.style.width = "30%";
         expandButton.setAttribute("data_expanded", false);
+        newNote.style.display = "none";
     }
 
 })
