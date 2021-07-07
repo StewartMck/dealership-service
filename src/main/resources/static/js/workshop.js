4fc99231cb7389a0f7bb7797b9b39b415bdf6bc4
Stewart McKinlay <stewart.mckinlay@gmail.com>
2021-07-07 14:12:43 -0600

    Add new note functionality

--- src/main/resources/static/js/workshop.js
+++ src/main/resources/static/js/workshop.js
@@ -2,19 +2,29 @@
 const expandButton = document.getElementById("expand");
 const table = document.getElementsByClassName("vehicles")[0];
 const edit = document.getElementsByClassName("edit-container")[0];
-const newNote = document.getElementById("new-note");
+const newNoteContainer = document.getElementById("new-note");
 
 expandButton.addEventListener("click", ()=>{
     if(expandButton.getAttribute("data_expanded") === 'false') {
         table.style.display = "none";
         edit.style.width = "80%";
         expandButton.setAttribute("data_expanded", true);
-        newNote.style.display = "flex";
+        newNoteContainer.style.display = "flex";
     } else {
         table.style.display = "table";
         edit.style.width = "30%";
         expandButton.setAttribute("data_expanded", false);
-        newNote.style.display = "none";
+        newNoteContainer.style.display = "none";
     }
 
 })
+
+const newNoteButton = newNoteContainer.querySelector("button")
+
+newNoteButton.addEventListener("click", ()=> {
+    const newNote= newNoteContainer.querySelector("input").value;
+    const workshopNotes = edit.querySelector("#workshop");
+    workshopNotes.value += `[${new Date().toLocaleString()}]: ${newNote}\n`;
+    })
+
+
